61987cbf913de724e0aef4872ba12459eec0959d
AndresFUlloa <a.ulloar@uniandes.edu.co>
2023-09-23 19:25:21 -0500

    Creacion Rama Develop

--- shareAlarms/src/app/event/event-create/event-create.component.ts
+++ shareAlarms/src/app/event/event-create/event-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Event } from '../event';
 
 @Component({
   selector: 'app-event-create',
@@ -8,11 +9,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EventCreateComponent implements OnInit {
 
+  eventForm!: FormGroup;
+  days!: number[];
 
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit() {this.eventForm = this.formBuilder.group({
+      description: ["", Validators.required],
+      date: ["", Validators.required],
+      time: ["", Validators.required]
+    });
+  }
 
-  ngOnInit() {
+  modifyDays(day: number){
+    if (!this.days.includes(day)) {
+      this.days.push(day);
+    } else {
+      this.days.splice(this.days.indexOf(day), 1);
+    }
+  }
+
+  crearEvento(event: Event){
+    event.dias = this.days;
   }
 
 }
